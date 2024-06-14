0979a06d8a7703aa0080c711129836a60428c2f6
Elisha-Tetteyfio <tetteyfioelisha@gmail.com>
2024-06-14 15:13:19 +0000

    Start up USSD

--- lib/hd_plus_ussd.ex
+++ lib/hd_plus_ussd.ex
@@ -10,8 +10,8 @@ defmodule HdPlusUssd do
 
   def send_response(conn, response_status, response) do
     conn
-    |> put_resp_header("content-type", "application/json")
-    |> send_resp(response_status, Poison.encode!(response))
+    |> put_resp_header("content-type", "text/plain")
+    |> send_resp(response_status, response)
   end
 
   get "/" do
@@ -24,11 +24,9 @@ defmodule HdPlusUssd do
     {:ok, body, conn} = read_body(conn)
     {:ok, parsed_body} = Poison.decode(body)
 
-    case Menu.MainMenu.select_menu(parsed_body) do
-      {:ok, response} ->
-        send_response(conn, 200, response)
-      _ ->
-        send_response(conn, 200, %{ussd_body: "System down", msg_type: '2'})
-    end
+    parsed_body = Map.put(parsed_body, "msg_type", "1")
+    parsed_body = Map.put(parsed_body, "ussd_body", "Welcome to HD+")
+
+    send_response(conn, 200, Poison.encode!(parsed_body))
   end
 end
